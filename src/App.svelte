7af00bd9f69dd3f837ba39b3b5f2077367ace44d
mattiasinokuchi <8-b@tuta.io>
2021-03-15 15:04:13 +0100

    added due date and number o todos

--- src/App.svelte
+++ src/App.svelte
@@ -4,28 +4,35 @@
     import Todo from './Todo.svelte';
 
     let newTodoText = '';
+    let newTodoDate = null;
+    let currSize = 0;
     const Todos = new Set();
 
     function addTodo() {
         const todo = new Todo({
             target: document.querySelector('#main'),
             props: {
-                num: Todo.size,
+                num: Todo.size + 1,
+                dueDate: newTodoDate,
                 description: newTodoText
             }
         });
         newTodoText = '';
+        newTodoDate = null;
         todo.$on('remove', () => {
             Todos.delete(todo); // reset text
+            currSize = Todos.size;
             todo.$destroy(); //prevent memory leak
         });
         Todos.add(todo);
+        currSize = Todos.size;
     }
 </script>
 
 <style></style>
 
-<h1>Todo Application!</h1>
+<h1>Todo Application! <span> Current number of Todos: {currSize}</span></h1>
 <ul id="main"></ul>
 <button on:click={addTodo}>Add Todo</button>
 <input type="text" bind:value={newTodoText}/>
+<input type="date" bind:value={newTodoDate} />
