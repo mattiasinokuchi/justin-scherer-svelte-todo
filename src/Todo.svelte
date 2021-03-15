71f3f10da7e164ba0bd4febfda658d1cd0a7cf88
mattiasinokuchi <8-b@tuta.io>
2021-03-15 16:22:07 +0100

    added transition

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -3,6 +3,8 @@
 <svelte:options accessors={true} />	<!-- App.svelte gets access and can change the properties below -->
 <script>
 	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
+
 	export let completed = false;
 	export let num = null;
 	export let description = null;
@@ -21,8 +23,10 @@
 	}
 </style>
 
-<li class:completed class:hidden>
-	Task {num}: {description} - Due on {dueDate}
-	<input type="checkbox" bind:checked={completed} />
-	<button on:click="{() => dispatch('remove', null)}">Remove</button>
-</li>
+{#if !hidden}
+	<li in:fade out:fade class:completed class:hidden>
+		Task {num}: {description} - Due on {dueDate}
+		<input type="checkbox" bind:checked={completed} />
+		<button on:click="{() => dispatch('remove', null)}">Remove</button>
+	</li>	
+{/if}
