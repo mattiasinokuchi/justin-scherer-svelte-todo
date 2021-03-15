26cee50eea9001e59278d1c641d5b0cd42c7e617
mattiasinokuchi <8-b@tuta.io>
2021-03-15 14:05:08 +0100

    new file

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+    import Todo from './Todo.svelte';
+
+    let newTodoText = '';
+    const Todos = new Set();
+
+    function addTodo() {
+        const todo = new Todo({
+            target: document.querySelector('#main'),
+            props: {
+                num: Todo.size,
+                description: newTodoText
+            }
+        });
+        newTodoText = '';
+        todo.$on('remove', () => {
+            Todos.delete(todo);
+            todo.$destroy();
+        });
+        Todos.add(todo);
+    }
+</script>
