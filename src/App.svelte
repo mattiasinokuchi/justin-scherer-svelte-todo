d99f3dc3ecda224b5fffee64e39242c086058274
mattiasinokuchi <8-b@tuta.io>
2021-03-15 13:31:33 +0100

    added list

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+	import { createEventDispatcher } from 'svelte';
+	export let completed;
+	export let num;
+	export let description;
+
+	const dispatch = createEventDispatcher();
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.completed {
+		text-decoration: line-through;
 	}
 </style>
+
+<li class:completed>
+	Task {num}: {description}
+	<input type="checkbox" bind:checked={completed} />
+	<button on:click="{() => dispatch('remove', null)}">Remove</button>
+</li>
