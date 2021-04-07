a9b5c57faa1e87c08bcac334f5a206edaabc670f
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-04-07 06:31:25 +0200

    changed counter for todos

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,25 @@
 <!-- This file contain logic for the whole application -->
-
 <script>
-    import Todo from './Todo.svelte';
-    import { completed, overdue } from './stores.js';
+    import { completed, overdue } from "./stores.js";
+    import Todo from "./Todo.svelte";
 
-    let newTodoText = '';
+    let newTodoText = "";
     let newTodoDate = null;
-    let currSize = 0;
     const Todos = new Set();
+    let currSize = Todos.size;
 
     function addTodo() {
         const todo = new Todo({
-            target: document.querySelector('#main'),
+            target: document.querySelector("#main"),
             props: {
-                num: currSize + 1,
+                num: Todos.size + 1,
                 dueDate: newTodoDate,
-                description: newTodoText
-            }
+                description: newTodoText,
+            },
         });
-        handleHide(todo);
-        newTodoText = '';
+        newTodoText = "";
         newTodoDate = null;
-        todo.$on('remove', () => {
+        todo.$on("remove", () => {
             Todos.delete(todo); // reset text
             currSize = Todos.size;
             todo.$destroy(); //prevent memory leak
@@ -33,7 +31,8 @@
     function handleHide(item) {
         const currDate = Date.now();
         if (completed && overdue) {
-            item.hidden = !item.completed || new Date(item.dueDate).getTime() > currDate;
+            item.hidden =
+                !item.completed || new Date(item.dueDate).getTime() > currDate;
             return;
         }
         if (completed) {
@@ -54,18 +53,18 @@
     }
 </script>
 
-<style></style>
-
 <h1>Todo Application! <span> Current number of Todos: {currSize}</span></h1>
-<ul id="main"></ul>
+<ul id="main" />
 <button on:click={addTodo}>Add Todo</button>
-<input type="text" bind:value={newTodoText}/>
+<input type="text" bind:value={newTodoText} />
 <input type="date" bind:value={newTodoDate} />
 <label>
-    <input type="checkbox" bind:checked={$completed} on:change={handleFilter}/>
+    <input type="checkbox" bind:checked={$completed} on:change={handleFilter} />
     Completed
 </label>
 <label>
-    <input type="checkbox" bind:checked={$overdue} on:change={handleFilter}>
+    <input type="checkbox" bind:checked={$overdue} on:change={handleFilter} />
     Overdue
 </label>
+
+<style></style>
